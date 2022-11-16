14de66b4882540d3800477b302aef2c3711e5d9d
Diogo Pedroso <pedrosod095@gmail.com>
2022-11-16 14:02:52 -0300

    cadastro de paciente

--- projetopim4.c
+++ projetopim4.c
@@ -1,6 +1,71 @@
 #include<stdio.h>
 #include<string.h>
 
+void get_patient_info(){
+
+    char nome[20];
+
+    printf("Digite o nome: ");
+    scanf("%s", nome);
+
+    char cpf[12];
+
+    printf("Insira o cpf: ");
+    scanf("%s", cpf);
+
+    char nascimento[10];
+
+    printf("Digite a data de nascimento: ");
+    scanf("%s", nascimento);
+
+    char telefone[13];
+
+    printf("Digite o telefone: ");
+    scanf("%s", telefone);
+
+    char diagnostico[12];
+
+    printf("Digite a data do diagnostico: ");
+    scanf("%s", diagnostico);
+
+    char comorbidades[40];
+
+    printf("Insira qual comorbidade do paciente: ");
+    scanf("%s", comorbidades);
+
+    char rua[15];
+
+    printf("Digite o número da casa: \n);
+    scanf("%s", rua);
+
+    char bairro[15];
+
+    printf("Digite o bairro: ");
+    scanf("%s", bairro);
+
+    char cidade[15];
+
+    printf("Digite a cidade: ");
+    scanf("%s", cidade);
+
+    char estado[15];
+
+    printf("Digite o estado: ");
+    scanf("%s", estado);
+
+    char CEP[12];
+
+    printf("Digite o CEP: ");
+    scanf("%s", CEP);
+
+
+    char Email[20];
+
+    printf("Digite o Email: ");
+    scanf("%s", Email);
+}
+
+
 int login(){
 
     char login[20];
